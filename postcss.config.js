09e1d6392ae5302cc3132c9dee8b97ed2c8f7866
tiennguyen <tiennguyen@coffeemugvn.com>
2021-08-03 18:14:43 +0700

    Fix import

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('tailwindcss/nesting')(require('postcss-nesting')),
-      require('tailwindcss'),
-      require('postcss-preset-env')({
-        features: { 'nesting-rules': false }
-      }),  
-    ],
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   }
